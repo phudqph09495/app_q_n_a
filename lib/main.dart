d9e1bbcde772e1528d7a1c2b11e6e7b0c24758d2
phudqph09495 <phudqph09495@fpt.edu.vn>
2022-06-23 11:56:02 +0700

    Đoàn Quang Phú-Update code 23/06/2022
    Fix trang đăng ký

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
           Locale('vi'),
         ],
         locale: Locale('vi'),
-        home: LoginScreen(),
+        home: ScreenHome(),
       ),
     );
   }
