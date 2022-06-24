8aa9c580679b812a35e444a61e5eb39b8a64b67b
phudqph09495 <phudqph09495@fpt.edu.vn>
2022-06-24 08:09:20 +0700

    Đoàn Quang Phú-Update code 23/06/2022
    Fix Giao diện

---
+++ lib/validator.dart
@@ -0,0 +1,47 @@
+class ValidatorApp{
+  static checkNull({String? text, bool isTextFiled = false}){
+    if(text == "null" || text == null || text == ""){
+      return isTextFiled ?  "Không bỏ trống" : "Đang cập nhật";
+    }else{
+      return isTextFiled ? null : text;
+    }
+  }
+  static checkPass({String? text, String? text2, bool isSign = false}){
+    if(text == "null" || text == "" || text == null){
+      return "Không bỏ trống";
+    }
+    else if(text.length < 6){
+      return "Mật khẩu phải lớn hơn 6 lý tự";
+    }
+    else if(text != text2 && isSign){
+      return "Mật khẩu mật khẩu không khớp";
+    }
+    else{
+      return  null;
+    }
+  }
+  static checkPhone({String? text,}){
+
+    if(text == "null" || text == null || text == ""){
+      return "Không bỏ trống";
+    }
+    else if(text.length != 10 || !text.startsWith("0")){
+      return "Số điện thoại không đúng định dạng";
+    }
+    else{
+      return  null;
+    }
+  }
+  static checkEmail({String? text,}){
+    var isEmail = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if(text == "null" || text == null || text == ""){
+      return "Không bỏ trống";
+    }
+    else if(!isEmail.hasMatch(text)){
+      return "Email không đúng định dạng";
+    }
+    else{
+      return  null;
+    }
+  }
+}
